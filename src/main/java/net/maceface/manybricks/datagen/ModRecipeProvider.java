43e58da278ca1b2f4959dfa2da9011bbd4baf934
Maceface823m <keefemaceo2@gmail.com>
2025-02-19 08:17:17 -0800

    js saving so it doesnt delete when im onr

--- src/main/java/net/maceface/manybricks/datagen/ModRecipeProvider.java
+++ src/main/java/net/maceface/manybricks/datagen/ModRecipeProvider.java
@@ -2,9 +2,12 @@ package net.maceface.manybricks.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.maceface.manybricks.block.ModBlocks;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.registry.RegistryWrapper;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
@@ -14,6 +17,11 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(RecipeExporter recipeExporter) {
+
+        List<ItemConvertible> STONE_CUTTABLE = List.of(ModBlocks.BRICK_TILES,
+                ModBlocks.DOUBLE_BASKETWEAVE_BRICKS, ModBlocks.ENGLISH_BOND_BRICKS, ModBlocks.FLEMMISH_BOND_BRICKS,
+                ModBlocks.HERRINGBONE_BRICKS, ModBlocks.SOLDIER_COURSE_BRICKS, ModBlocks.SOLDIER_TOPPED_STACKED_BOND_BRICKS,
+                ModBlocks.STACKED_BOND_BRICKS);
 
     }
 }
